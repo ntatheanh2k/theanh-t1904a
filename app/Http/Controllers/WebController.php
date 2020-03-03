5b53ae0af49d4c9a883c8076451d8731b6aa23fc
phamthai12316 <phamthai12316@gmail.com>
2020-03-03 23:18:03 +0700

    theanh

---
+++ app/Http/Controllers/WebController.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+use \Illuminate\Http\Request;
+
+
+class WebController
+{
+    public function homefoods(){
+        return view('');
+    }
+    public function shop(){
+        return view('');
+    }
+    public function productDetail(){
+        return view('');
+    }
+}
